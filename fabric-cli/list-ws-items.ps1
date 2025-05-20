086c130890cda9bf8d0b1b8a5f36b75eaa42c632
Alejandra Guillen <4201052+aleguillen@users.noreply.github.com>
2025-05-20 11:18:12 -0500

    Export data to CSV if specified

--- fabric-cli/list-ws-items.ps1
+++ fabric-cli/list-ws-items.ps1
@@ -7,15 +7,24 @@
 # Importing Helper functions
 . ./utils.ps1
 
-# Login to Microsoft Fabric CLI if needed - uncomment the line below if you haven't logged in yet
-# fab auth login
+$AuthType = "Interactive" # Set to  'Identity', 'Interactive', or 'ServicePrincipal'
+$global:clientId = "<client_id>" # Replace with your client ID
+$global:clientSecret = "<client_secret>" # Replace with your client secret
+$global:tenantId = "<tenant_id>" # Replace with your tenant ID
+$global:refreshTokenAfterDate = (Get-Date)
+
+FabricLogin -AuthType $AuthType 
 
 $workspaceName = "<WORKSPACE NAME>" # Replace with your workspace name if Empty, it will list all workspaces
 $listItems = $true # Set to $true to list items in the workspace
+$exportCSV = $true # Set to $true to export items to CSV
 $pauseEachWorkspace = $false # Set to $true to pause after each workspace
 
 # List all workspaces for the current user
 $workspaces = (fab api -X get workspaces --show_headers | ConvertFrom-Json).text.value
+if ($exportCSV) {
+    $workspaces | Export-Csv -Path ".\workspaces.csv" -NoTypeInformation
+}
 
 if ($workspaceName) {
     $workspaces = $workspaces | Where-Object { $_.displayName -eq $workspaceName }
@@ -40,6 +49,15 @@ foreach ($ws in $workspaces) {
     $sortedFolders = SortFolders -Folders $wsFolders
     WriteTable ($sortedFolders | Select-Object id, displayName,RelativePath)
 
+    # Adding Subfolder information to items
+    $wsItems | ForEach-Object {
+        $folderId = $_.folderId
+        if ($folderId) {
+            $subfolder = ($sortedFolders | Where-Object {$_.id -eq $folderId}).RelativePath
+            $_ | Add-Member -MemberType NoteProperty -Name subfolder -Value $subfolder -Force
+        }
+    }
+
     $itemsCountByType = $wsItems | Group-Object type | ForEach-Object {
         [PSCustomObject]@{
             Type  = $_.Name
@@ -48,10 +66,16 @@ foreach ($ws in $workspaces) {
     }
     WriteSubHeaderMessage "Items Count by Type"
     WriteTable $itemsCountByType
+    # $wsItemsWithSubfolders = $wsItems | Sort-Object Type, DisplayName | Select-Object DisplayName, Type, Subfolder@{Name="Subfolder";Expression={if($_.folderId){$folderId = $_.folderId; ($sortedFolders | Where-Object {$_.id -eq $folderId}).RelativePath}else{""}}}
 
     if ($listItems) {
         WriteSubHeaderMessage "Items Sorted by Type and Name"
-        WriteTable ($wsItems | Sort-Object Type, DisplayName | Select-Object DisplayName, Type, @{Name="Subfolder";Expression={if($_.folderId){$folderId = $_.folderId; ($sortedFolders | Where-Object {$_.id -eq $folderId}).RelativePath}else{""}}})
+        WriteTable ($wsItems | Sort-Object Type, DisplayName | Select-Object DisplayName, Type, Subfolder)
+    }
+
+    if ($exportCSV) {
+        Write-Host "Exported items to .\$($wsName)_items.csv"
+        $wsItems | Select-Object id, displayName, type, description, workspaceId, folderId, subfolder | Export-Csv -Path ".\$($wsName)_items.csv" -NoTypeInformation
     }
 
     if ($pauseEachWorkspace) {
