13cac5554de027a6091157d6e946b59b09e028f3
Alejandra Guillen <4201052+aleguillen@users.noreply.github.com>
2025-05-22 21:36:56 -0500

    Adding exportCSV option to Export script

--- fabric-cli/export-ws-items.ps1
+++ fabric-cli/export-ws-items.ps1
@@ -13,8 +13,8 @@ $global:clientSecret = "<client_secret>" # Replace with your client secret
 $global:tenantId = "<tenant_id>" # Replace with your tenant ID
 
 $workspaceName = "<WORKSPACE NAME>" # Replace with your workspace name
-
 $listItems = $true # Set to $true to list items in the workspace
+$exportCSV = $true # Set to $true to export items to CSV
 $pauseEachWorkspace = $false # Set to $true to pause after each workspace
 
 $timestamp = (Get-Date -Format "yyyyMMddHHmm") # You can specify a custom timestamp if needed to re-run the script where you left off
@@ -33,6 +33,10 @@ FabricLogin -AuthType $AuthType
 # List all workspaces for the current user
 $workspaces = (fab api -X get workspaces --show_headers | ConvertFrom-Json).text.value
 
+if ($exportCSV) {
+    $workspaces | Export-Csv -Path "$exportParentPath\workspaces.csv" -NoTypeInformation -Force:$replaceFileIfExists
+}
+
 if ($workspaceName) {
     $workspaces = $workspaces | Where-Object { $_.displayName -eq $workspaceName }
 }
@@ -46,7 +50,7 @@ foreach ($ws in $workspaces) {
 
     $wsItems = (fab api workspaces/$wsId/items | ConvertFrom-Json).text.value
 
-    Write-Host "Total items in workspace '$wsName': $($wsItems.Count)" 
+    WriteMessage "Total items in workspace '$wsName': $($wsItems.Count)" 
     if ($wsItems.Count -eq 0) {
         continue
     }
@@ -78,9 +82,14 @@ foreach ($ws in $workspaces) {
         WriteSubHeaderMessage "Items Sorted by Type and Name"
         WriteTable ($wsItems | Sort-Object Type, DisplayName | Select-Object DisplayName, Type, Subfolder)
     }
+    
+    if ($exportCSV) {
+        Write-Host "Exported items to $exportParentPath\$($wsName)_items.csv"
+        $wsItems | Select-Object id, displayName, type, description, workspaceId, folderId, subfolder | Export-Csv -Path "$exportParentPath\$($wsName)_items.csv" -NoTypeInformation
+    }
 
     # Create new directory for exports if it doesn't exist
-    $exportPath = "$exportParentPath\$wsFullName"
+    $exportPath = "$exportParentPath\$wsName"
     if (-not (Test-Path -Path "$exportPath")) {
         New-Item -ItemType Directory -Path "$exportPath" | Out-Null
     }
@@ -91,15 +100,15 @@ foreach ($ws in $workspaces) {
         $folderPath = Join-Path -Path $exportPath -ChildPath $folder.RelativePath
         if (-not (Test-Path -Path $folderPath)) {
             New-Item -ItemType Directory -Path $folderPath | Out-Null
-            Write-Host "New folder: $folderPath created."
+            WriteMessage "New folder: $folderPath created."
         }
     }
 
     WriteSubHeaderMessage "Exporting items to folder: $exportPath"
-    Write-Host "When you export item definition, the sensitivity label is not a part of the definition." -ForegroundColor Yellow
+    WriteMessage "When you export item definition, the sensitivity label is not a part of the definition." -ForegroundColor Yellow
 
     if ($exportItemTypes.Count -eq 0) {
-        Write-Host "Exporting all supported items in the workspace."
+        WriteMessage "Exporting all supported items in the workspace."
         $exportItemTypes = @("CopyJob","DataPipeline","Eventhouse","Eventstream","KQLDashboard","KQLDatabase","KQLQueryset","MirroredDatabase","MountedDataFactory","Notebook","Reflex","Report","SemanticModel","SparkJobDefinition","VariableLibrary") # Supported item types for export
         
         # Commenting full export for now since it doesn't support folders
@@ -107,18 +116,19 @@ foreach ($ws in $workspaces) {
 
     } 
 
-    Write-Host "Exporting items of types: $($exportItemTypes -join ', ')" 
+    WriteMessage "Exporting items of types: $($exportItemTypes -join ', ')" 
 
     $exportableItems = $wsItems| Where-Object { $exportItemTypes -contains $_.Type }
     $notExportedItems = $wsItems | Where-Object { -not ($exportItemTypes -contains $_.Type) }
+    $successfullyExportedItems = @()
     $erroredItems = @()
 
     $itemsCount = $exportableItems.Count
+    WriteMessage "Total exportable items in workspace '$wsName': $itemsCount/$($wsItems.Count)" 
+    AddLogMessage -Message "Total exportable items in workspace '$wsName': $itemsCount/$($wsItems.Count)"
     
     for ($i = 0; $i -lt $itemsCount; $i++) {
         $item = $exportableItems[$i]
-        FabricLogin -AuthType $AuthType # Re-login to ensure the token is valid
-
         $itemFullName = $item.DisplayName + "." + $item.Type
         $itemName = $item.DisplayName
         $itemType = $item.Type
@@ -129,12 +139,11 @@ foreach ($ws in $workspaces) {
             if ($itemFolder) {
                 $itemExportPath = Join-Path -Path $exportPath -ChildPath $itemFolder.RelativePath
             } else {
-                Write-Host "Folder for item '$itemName' not found. Exporting to root folder." -ForegroundColor Yellow
+                WriteMessage "Folder for item '$itemName' not found. Exporting to root folder." -ForegroundColor Yellow
             }
         }
         
-        Write-Host "Exporting item [$($i+1)/$itemsCount]: $itemExportPath\$itemFullName" -ForegroundColor Cyan
-        AddLogMessage -Message "Exporting item [$($i+1)/$itemsCount]: $itemExportPath\$itemFullName"
+        WriteMessage "Exporting item [$($i+1)/$itemsCount]: $itemExportPath\$itemFullName" -ForegroundColor Cyan
 
         try {
             if (-not (Test-Path -Path (Join-Path -Path $itemExportPath -ChildPath $itemFullName))) {
@@ -142,28 +151,35 @@ foreach ($ws in $workspaces) {
 
                 if ($LASTEXITCODE -gt 0) {
                     $errorMessage = (fab export "$wsFullName/$itemFullName" -o $itemExportPath -f) | Out-String
-                    Write-Host "Failed to export item '$itemName' of type '$itemType'. Error Code: $LASTEXITCODE" -ForegroundColor Red
-                    Write-Host $errorMessage -ForegroundColor Red
-                    
-                    AddLogMessage -Message "Failed to export item '$itemName' of type '$itemType'. Error Code: $LASTEXITCODE"
-                    AddLogMessage -Message $errorMessage
-
-                    $erroredItems += $item
+                    WriteMessage "Failed to export item '$itemName' of type '$itemType'. Error Code: $LASTEXITCODE" -ForegroundColor Red
+                    WriteMessage $errorMessage -ForegroundColor Red
+                    $eItem = $item
+                    $eItem | Add-Member -MemberType NoteProperty -Name errorMessage -Value $errorMessage -Force
+                    $erroredItems += $eItem
                 }
                 else {
-                    Write-Host "Item '$itemName' of type '$itemType' exported successfully." -ForegroundColor Green
-                    AddLogMessage -Message "Item '$itemName' of type '$itemType' exported successfully."
+                    WriteMessage "Item '$itemName' of type '$itemType' exported successfully." -ForegroundColor Green
+                    $successfullyExportedItems += $item
                 }
 
             }
             else {
-                Write-Host "Item '$itemFullName' already exists in the export path. Skipping export." -ForegroundColor Yellow
-                AddLogMessage -Message "Item '$itemFullName' already exists in the export path. Skipping export."
+                WriteMessage "Item '$itemFullName' already exists in the export path. Skipping export." -ForegroundColor Yellow
+                $successfullyExportedItems += $item
             }
         } catch {
-            Write-Host "Failed to export item '$itemName' of type '$itemType'. Error: $_" -ForegroundColor Red
-            AddLogMessage -Message "Failed to export item '$itemName' of type '$itemType'. Error: $_"
+            WriteMessage "Failed to export item '$itemName' of type '$itemType'. Error: $_" -ForegroundColor Red
+            $eItem = $item
+            $eItem | Add-Member -MemberType NoteProperty -Name errorMessage -Value $_ -Force
+            $erroredItems += $eItem
         }
+
+        FabricLogin -AuthType $AuthType # Re-login to ensure the token is valid
+    }
+
+    if ($exportCSV) {
+        WriteMessage "Exported successfully exported items to $exportParentPath\$($wsName)_successfully_exported_items.csv"
+        $successfullyExportedItems | Select-Object id, displayName, type, description, workspaceId, folderId, subfolder | Export-Csv -Path "$exportParentPath\$($wsName)_successfully_exported_items.csv" -NoTypeInformation
     }
 
     if ($notExportedItems.Count -gt 0) {
@@ -177,6 +193,12 @@ foreach ($ws in $workspaces) {
         
         WriteSubHeaderMessage "Items Not Exported Sorted by Type and Name"
         WriteTable ($notExportedItems | Sort-Object Type, DisplayName | Select-Object DisplayName, Type, Subfolder)
+
+        if ($exportCSV) {
+            WriteMessage "Exported items not exported to $exportParentPath\$($wsName)_not_exported_items.csv"
+            $notExportedItems | Select-Object id, displayName, type, description, workspaceId, folderId, subfolder | Export-Csv -Path "$exportParentPath\$($wsName)_not_exported_items.csv" -NoTypeInformation
+        }
+        
     } 
 
     if ($erroredItems.Count -gt 0) {
@@ -190,13 +212,22 @@ foreach ($ws in $workspaces) {
 
         WriteSubHeaderMessage "Errored Items List Sorted by Type and Name"
         WriteTable ($erroredItems | Sort-Object Type, DisplayName | Select-Object DisplayName, Type, Subfolder)
+
+        if ($exportCSV) {
+            WriteMessage "Exported errored items to $exportParentPath\$($wsName)_errored_items.csv"
+            $erroredItems | Select-Object id, displayName, type, description, workspaceId, folderId, subfolder, errorMessage | Export-Csv -Path "$exportParentPath\$($wsName)_errored_items.csv" -NoTypeInformation
+        }
     }
     
+    WriteMessage "Total successfully exported items: $($successfullyExportedItems.Count)/$($wsItems.Count)" -ForegroundColor Green
+    WriteMessage "Total errored items: $($erroredItems.Count)/$($wsItems.Count)" -ForegroundColor Red
+    WriteMessage "Total not exported items: $($notExportedItems.Count)/$($wsItems.Count)" -ForegroundColor Yellow
+
     if ($pauseEachWorkspace) {
-        Write-Host "Press Enter to continue to the next workspace or Ctrl+C to stop execution..."
+        WriteMessage "Press Enter to continue to the next workspace or Ctrl+C to stop execution..."
     }
 }
 
-Write-Host "Export completed. Check the log file at $global:LogFilePath for details." -ForegroundColor Green
+WriteMessage "Export completed. Check the log file at $global:LogFilePath for details." -ForegroundColor Green
 
 # End of script
